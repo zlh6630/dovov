dbb2fd7b350ac93119cf2612c9fd6ca0c2bc7610
zhang <zlh6630@qq.com>
2021-09-24 22:43:51 +0800

    first commit

---
+++ cura/ApplicationMetadata.py
@@ -0,0 +1,58 @@
+# Copyright (c) 2020 Ultimaker B.V.
+# Cura is released under the terms of the LGPLv3 or higher.
+
+# ---------
+# General constants used in Cura
+# ---------
+DEFAULT_CURA_APP_NAME = "DOVOV Slicer"
+DEFAULT_CURA_DISPLAY_NAME = "DOVOV Slicer"
+DEFAULT_CURA_VERSION = "4.11.0"
+DEFAULT_CURA_BUILD_TYPE = ""
+DEFAULT_CURA_DEBUG_MODE = False
+
+# Each release has a fixed SDK version coupled with it. It doesn't make sense to make it configurable because, for
+# example Cura 3.2 with SDK version 6.1 will not work. So the SDK version is hard-coded here and left out of the
+# CuraVersion.py.in template.
+CuraSDKVersion = "7.7.0"
+
+try:
+    from cura.CuraVersion import CuraAppName  # type: ignore
+    if 1 == 1:
+        CuraAppName = DEFAULT_CURA_APP_NAME
+except ImportError:
+    CuraAppName = DEFAULT_CURA_APP_NAME
+
+try:
+    from cura.CuraVersion import CuraVersion  # type: ignore
+    if 1 == 1:
+        CuraVersion = DEFAULT_CURA_VERSION
+except ImportError:
+    CuraVersion = DEFAULT_CURA_VERSION  # [CodeStyle: Reflecting imported value]
+
+# CURA-6569
+# This string indicates what type of version it is. For example, "enterprise". By default it's empty which indicates
+# a default/normal Cura build.
+try:
+    from cura.CuraVersion import CuraBuildType  # type: ignore
+except ImportError:
+    CuraBuildType = DEFAULT_CURA_BUILD_TYPE
+
+try:
+    from cura.CuraVersion import CuraDebugMode  # type: ignore
+except ImportError:
+    CuraDebugMode = DEFAULT_CURA_DEBUG_MODE
+
+# CURA-6569
+# Various convenience flags indicating what kind of Cura build it is.
+__ENTERPRISE_VERSION_TYPE = "enterprise"
+IsEnterpriseVersion = CuraBuildType.lower() == __ENTERPRISE_VERSION_TYPE
+
+try:
+    from cura.CuraVersion import CuraAppDisplayName  # type: ignore
+    if CuraAppDisplayName == "":
+        CuraAppDisplayName = DEFAULT_CURA_DISPLAY_NAME
+    if IsEnterpriseVersion:
+        CuraAppDisplayName = CuraAppDisplayName + " Enterprise"
+
+except ImportError:
+    CuraAppDisplayName = DEFAULT_CURA_DISPLAY_NAME
